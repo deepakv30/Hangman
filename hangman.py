9739595f3f092dbb619aeb71fe964dc6eadad7cc
deepakv30 <deeptech30@gmail.com>
2019-04-13 23:42:41 +0530

    First Commit for hangman game

---
+++ hangman.py
@@ -0,0 +1,46 @@
+import random
+
+Words = ('python', 'david', 'honor', 'room')
+word = random.choice(Words)
+trigger = 0
+wordGuess = '*' * len(word)
+wordGuessList = list(wordGuess)
+print('Welcome to play Hangman')
+num = int(input('Enter number of choices in which you will guess : '))
+# for x in word:
+#     print('*', end=' ')
+print('\n You have to guess a word that has ' + str(len(word)) + ' letters : ')
+print(wordGuessList)
+while num > 0:
+    guess = input(str(num) + ' guess remaining : ')
+    for x in word:
+        if guess == x:
+            i = word.index(x)
+            trigger = 1
+            if wordGuessList[i] == '*':
+                wordGuessList[i] = x
+            else:
+                continue
+        else:
+            continue
+    print(wordGuessList)
+    if trigger == 1:
+        if "".join(wordGuessList) == word:
+            print('You Win the game!')
+            break
+        trigger = 0
+    else:
+        num = num-1
+
+if "".join(wordGuessList) != word:
+    print('You lose the game! Try Again')
+
+
+
+
+
+
+
+
+
+
